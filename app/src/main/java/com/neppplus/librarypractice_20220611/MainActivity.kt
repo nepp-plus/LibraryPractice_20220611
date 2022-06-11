39b3ec572e8da42e218ff0203cfccaecbe650749
조경진 <cho881020@gmail.com>
2022-06-11 13:36:43 +0900

    이미지뷰에 클릭 이벤트 부여

--- app/src/main/java/com/neppplus/librarypractice_20220611/MainActivity.kt
+++ app/src/main/java/com/neppplus/librarypractice_20220611/MainActivity.kt
@@ -2,10 +2,33 @@ package com.neppplus.librarypractice_20220611
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
+
+    fun setupEvents() {
+
+        imgProfile.setOnClickListener {
+
+//            이미지뷰, 텍스트뷰, LinearLayout 등도 단순 클릭 이벤트 지원.
+
+//            별도의 사진을 크게 보는 액티비티로 이동.
+
+
+
+
+        }
+
+    }
+
+    fun setValues() {
+
+    }
+
 }
