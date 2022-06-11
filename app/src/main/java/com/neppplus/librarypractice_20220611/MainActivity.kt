0a47d717af08982c1c5ce702f4c8f18f2c7146c5
조경진 <cho881020@gmail.com>
2022-06-11 14:37:41 +0900

    실제로 권한 물어보기

--- app/src/main/java/com/neppplus/librarypractice_20220611/MainActivity.kt
+++ app/src/main/java/com/neppplus/librarypractice_20220611/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.neppplus.librarypractice_20220611
 
+import android.Manifest
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -62,6 +64,13 @@ class MainActivity : AppCompatActivity() {
 
             }
 
+
+//            계획서를 들고, 실제로 권한 확인
+
+            TedPermission.create()
+                .setPermissionListener(pl)
+                .setPermissions( Manifest.permission.CALL_PHONE )
+                .check()
 
 
         }
